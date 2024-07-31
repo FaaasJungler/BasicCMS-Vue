b9d2e8786b88ff630ecd05936b4a4ab363b02dc4
Mathieu Castonguay <mathcas3194@gmail.com>
2024-07-31 19:58:40 -0400

    Adding an hero section and adding it to the home page juste because i'm tire of the emptyness

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="hero">
+    <div class="hero-content">
+      <h1 class="hero-title">{{ title }}</h1>
+      <p class="hero-subtitle">{{ subtitle }}</p>
+      <a href="#" class="hero-button">{{ buttonText }}</a>
+    </div>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+    subtitle: String,
+    buttonText: String
+  }
+};
+
+</script>
+<style src="../assets/styles/Header.scss"></style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+   <Header
+      title="Your Animated Text Goes Here"
+      subtitle="A brief description of your content"
+      buttonText="Get Started"
+   />
   </div>
 </template>
 
 <script>
+import Header from "./Header.vue";
+
 export default {
   name: 'Home',
+  components: {Header},
 };
 </script>
 <style src="../assets/styles/Home.scss"></style>
